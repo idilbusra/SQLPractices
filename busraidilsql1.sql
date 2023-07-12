4e7a7f0dec735092cf77c70e4a64d4ef5a6b945e
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-12 20:23:57 +0300

    Data has been added to the table.

--- busraidilsql1.sql
+++ busraidilsql1.sql
@@ -8,3 +8,10 @@ create table greengrocer(
 
 select * from greengrocer;
 
+insert into greengrocer values('Strawberry' , 'kg' , 39.99, 1.4 );
+insert into greengrocer values('Banana' , 'kg' , 29.99, 1.4 );
+insert into greengrocer values('Watermelon' , 'kg' , 22.00, 1 );
+insert into greengrocer values('Kiwi' , 'kg' , 39.99, 1.4 );
+
+select * from greengrocer;
+
