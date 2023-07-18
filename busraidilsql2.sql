c4d797806be2d6157b3b58d8da5def67fd76ec69
busra idil <diyetisyenbusraidil@gmail.com>
2023-07-18 23:34:04 +0300

    Data has been added to the table.

--- busraidilsql2.sql
+++ busraidilsql2.sql
@@ -6,6 +6,16 @@ address varchar(15),
 age int
 );
 
+INSERT INTO people VALUES (1, 'Alex', 'Texas', 23);
+INSERT INTO people VALUES (2, 'James', 'Colorado', 43);
+INSERT INTO people VALUES (3, 'Maria', 'Utah', 19);
+INSERT INTO people VALUES (4, 'Susan', 'Oklahoma', 32);
+INSERT INTO people VALUES (5, 'Katty', 'Virginia', 27);
+INSERT INTO people VALUES (6, 'Tom', 'Virginia', 43);
+
+
+SELECT * FROM people;
+
 
 
 
